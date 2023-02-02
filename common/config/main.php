e8286545c77778ee770de9969f54fcd1cda22b2f
KurniaTanggang <kurniasitanggang1@gmail.com>
2023-02-02 12:42:56 +0700

    konfigurasi rbac

--- common/config/main.php
+++ common/config/main.php
@@ -4,10 +4,22 @@ return [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+        ],
+    ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager', // or use 'yii\rbac\DbManager'
+        ],
+        'user' => [
+            'identityClass' => 'mdm\admin\models\User',
+            'loginUrl' => ['admin/user/login'],
         ],
     ],
 ];
